809204d2f85e58f25b3b0dcb9d68e9a4d9613dfb
Jason Dansie <jasondansie@gmail.com>
2022-12-18 13:01:56 +0200

    fixes bird page

--- src/Birds.jsx
+++ src/Birds.jsx
@@ -1,6 +1,27 @@
 import './Birds.css';
+import Card from './Card';
 
 const Birds = (props) => {
+
+    const removeCardHandler = (name) => {
+        console.log("remove card clicked", name);
+        const updateArray = props.birds.filter(
+            (bird) => bird.name !== name
+        )
+        return updateArray;
+    }
+
+    const birdsList = props.birds.map((bird) => {
+        return (
+            <Card
+                key={bird.name}
+                name={bird.name}
+                likes={bird.likes}
+                removeCard={() => removeCardHandler(bird.name)}
+                addLikes={() => this.addlikeHandler(bird.likes)}
+            />
+        );
+    });
     return (
         <div>
             <h1>The Birds Page</h1>
@@ -8,7 +29,7 @@ const Birds = (props) => {
                 <label htmlFor="search">search</label>
                 <input type="text" name="search" onChange={props.searchHandler}></input>
             </div>
-            <div className='cards'>{props.birds}</div>
+            <div className='cards'>{birdsList}</div>
         </div>
     );
 }
